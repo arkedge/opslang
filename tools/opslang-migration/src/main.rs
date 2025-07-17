131b02f09e7d1a1dab24b081000a181e0cf944ee
sylide <71720482+s-ylide@users.noreply.github.com>
2025-07-17 15:58:46 +0900

    migration: use meta comment

--- tools/opslang-migration/src/main.rs
+++ tools/opslang-migration/src/main.rs
@@ -234,7 +234,7 @@ fn generate_metadata(include_comments: bool) -> String {
         let args: Vec<String> = env::args().collect();
         let command_line = args.join(" ");
 
-        metadata.push_str(&format!("# Migrated at {datetime} by: {command_line}\n"));
+        metadata.push_str(&format!("#! Migrated at {datetime} by: {command_line}\n"));
     }
 
     metadata
